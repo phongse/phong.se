868534f8df6c15e0c59749e6960bc3acce5f1586
Phong Tran <540580+phongse@users.noreply.github.com>
2022-10-10 23:12:29 +0200

    remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import About from "../components/About/About";
 import Contact from "../components/Contact/Contact";
 import Projects from "../components/Projects/Projects";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
