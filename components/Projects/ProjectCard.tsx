0f361d1e3f11bbabe408c936902787940b23f35e
Phong Tran <540580+phongse@users.noreply.github.com>
2023-01-21 23:22:44 +0100

    Fix image import for nextjs 13

--- components/Projects/ProjectCard.tsx
+++ components/Projects/ProjectCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
-import Image from "next/future/image";
+import Image from "next/image";
 
 type Props = {
   title: string;
